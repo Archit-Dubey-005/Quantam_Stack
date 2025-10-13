ea897072a3dddb7672cfec01176df795203f6d75
Archit Dubey <9879567157r@gmail.com>
2025-10-13 13:37:58 +0530

    Update Result.py
    changed to url from localhost:ip_add:3001 to render base url

--- Result.py
+++ Result.py
@@ -5,7 +5,7 @@ from io import StringIO
 # ====== 0. Load the trained model from merge2csv5.py ======
 # Make sure merge2csv5.py defines 'model' at the global level
 from merge2csv5 import model
-url = "http://192.168.56.1:3001/baseline-16day"
+url = https://quantam-stack.onrender.com/baseline-16day"
 
 # ====== 1. Fetch daily baseline CSV from backend route ======
 
